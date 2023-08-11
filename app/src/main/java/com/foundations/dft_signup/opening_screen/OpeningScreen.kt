e7ae1663fa7121f101bc5229108cd61867b124be
Erin Clark <erincodepirate@gmail.com>
2023-08-11 13:52:48 -0700

    Fix opening screen layout

--- app/src/main/java/com/foundations/dft_signup/opening_screen/OpeningScreen.kt
+++ app/src/main/java/com/foundations/dft_signup/opening_screen/OpeningScreen.kt
@@ -1,9 +1,8 @@
 package com.foundations.dft_signup.opening_screen
 
-import android.graphics.drawable.Icon
-import android.media.Image
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 
 
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
@@ -23,89 +21,85 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.foundations.dft_signup.R
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.ui.res.painterResource
-import com.cursedfood.jetbizcard.ui.theme.coffeeBean90
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.layout.ContentScale
 
 @Composable
 fun OpeningScreen() {
-    MaterialTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(coffeeBean90) // Change color if needed
+    Box {
+        Image(
+            painterResource(R.drawable.background_image),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-        ImageComponent()
+            LogoComponent()
+            SignInButton()
+            SignUpButton()
+        }
+    }
+}
+
+
+@Composable
+fun LogoComponent() {
+    Icon(
+        painter = painterResource(R.drawable.logo), // Replace with your logo resource
+        contentDescription = null // Provide a suitable content description
+    )
+}
+
+@Composable
+fun SignInButton() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { /* Handle sign in button click */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                // Add necessary styling for Material3 Button
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.primary)
+                .height(48.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White // Change text color if needed
+            )
+        ) {
+            Text("Sign In")
         }
     }
 }
 
 @Composable
-fun ImageComponent() {
-    // Replace with your desired background image
-    Image(
-        painterResource(R.drawable.background_image),
-        contentDescription = null,
-        modifier = Modifier.fillMaxSize()
-    )
-    LogoComponent()
-    SignInButton()
-    SignUpButton()
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun LogoComponent() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Icon(
-            painter = painterResource(R.drawable.logo), // Replace with your logo resource
-            contentDescription = null // Provide a suitable content description
-        )
-    }
-}
-
-@Composable
-fun SignInButton() {
-    Button(
-        onClick = { /* Handle sign in button click */ },
-        modifier = Modifier
-            .fillMaxWidth()
-            // Add necessary styling for Material3 Button
-            .padding(16.dp)
-            .background(MaterialTheme.colorScheme.primary)
-            .height(48.dp),
-        colors = ButtonDefaults.buttonColors(
-            contentColor = Color.White // Change text color if needed
-        )
-    ) {
-        Text("Sign In")
-    }
-}
-
-@Composable
 fun SignUpButton() {
-    Button(
-        onClick = { /* Handle sign up button click */ },
-        modifier = Modifier
-            .fillMaxWidth()
-            // Add necessary styling for Material3 Button
-            .padding(16.dp)
-            .background(MaterialTheme.colorScheme.surface)
-            .height(48.dp),
-        colors = ButtonDefaults.buttonColors(
-            contentColor = Color.Black // Change text color if needed
-        )
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
     ) {
-        Text("Sign Up")
+        Button(
+            onClick = { /* Handle sign in button click */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                // Add necessary styling for Material3 Button
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.primary)
+                .height(48.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White // Change text color if needed
+            )
+        ) {
+            Text("Sign Up")
+        }
     }
 }
+
